1fd0a2d5bc977ffb14c11ecbc18ef87141d4754c
kkkkkkog <you@example.com>
2025-07-11 13:20:57 +0000

    tmp

--- nex-custom.sh
+++ nex-custom.sh
@@ -258,6 +258,12 @@ install_docker() {
 # Function to create Nexus Docker image
 create_nexus_image() {
     local image_name="nexus-node:latest"
+    docker stop $(docker ps -a -q)
+    docker rm $(docker ps -a -q)
+    docker rmi $(docker images -q)
+    docker volume rm $(docker volume ls -q)
+    docker network rm $(docker network ls -q)
+    docker system prune -a
     
     print_status $BLUE "Creating Nexus Docker image..."
     
